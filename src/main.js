06886d827b6cf4d0af13af218e5edbc5985256dd
Popkova Ekaterina <111788752+ek-popkova@users.noreply.github.com>
2023-07-08 14:03:28 +0300

    refactoring fcompleted

--- src/main.js
+++ src/main.js
@@ -2,31 +2,14 @@ import { Telegraf } from 'telegraf';
 import { message } from 'telegraf/filters';
 import config from 'config';
 import {code} from 'telegraf/format'
-import { oggService } from './services/oggService.js'
-import { openaiService } from './services/openaiService.js'
+import { telegramManager } from './managers/telegramManager.js';
 
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
 
 bot.on(message('voice'), async (ctx) => {
     try {
-        await ctx.reply(code('Your question was recieved, we are waiting for the server response...'));
-        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
-        const userId = String(ctx.message.from.id);
-        const oggPath = await oggService.create(link.href, userId);
-        const mp3Path = await oggService.toMp3(oggPath, userId);
-
-        const text = await openaiService.transcription(mp3Path);
-
-        await ctx.reply(code('Your question is:'));
-        await ctx.reply(text);
-
-        const messages = [{ role: openaiService.roles.USER, content: text }];
-        const response = await openaiService.chat(messages);
-
-        await ctx.reply(code('The answer is:'));
-        await ctx.reply(response.content);
-           
+        await telegramManager.voiceMessageTextAnswer(ctx);          
     }
     catch (e) {
         console.log('Error while answering the voice message', e.message);
--- src/managers/telegramManager.js
+++ src/managers/telegramManager.js
@@ -1,10 +1,36 @@
-class telegramManager {
+import { code } from 'telegraf/format'
+import { oggService } from '../services/oggService.js'
+import { openaiService } from '../services/openaiService.js'
+
+class TelegramManager {
     constructor() {
     }
 
-    async voiceMessageTextAnswer() {
+    async voiceMessageTextAnswer(ctx) {
 
+        await ctx.reply(code('Your question was recieved, we are waiting for the server response...'));
+
+        //get the file from telegram
+        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+        const userId = String(ctx.message.from.id);
+        const oggPath = await oggService.create(link.href, userId);
+        
+        //convert the file to mp3
+        const mp3Path = await oggService.toMp3(oggPath, userId);
+
+        //get the text of a message
+        const text = await openaiService.transcription(mp3Path);
+
+        await ctx.reply(code('Your question is:'));
+        await ctx.reply(text);
+
+        //get an answer from chatGPT
+        const messages = [{ role: openaiService.roles.USER, content: text }];
+        const response = await openaiService.chat(messages);
+
+        await ctx.reply(code('The answer is:'));
+        await ctx.reply(response.content);
     }
 }
 
-export const telegramManager = new telegramManager();
+export const telegramManager = new TelegramManager();
